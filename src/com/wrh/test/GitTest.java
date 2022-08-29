a868f44d3f8ff8ba0811ab27450728feb4131468
王锐海 <wrhai94@163.com>
2022-08-29 13:43:53 +0800

    second commit

--- src/com/wrh/test/GitTest.java
+++ src/com/wrh/test/GitTest.java
@@ -7,5 +7,6 @@ package com.wrh.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
